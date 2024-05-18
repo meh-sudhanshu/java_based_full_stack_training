bb9577be60d5ce999cb2e80677bb98c543e58c50
sudhanshu kumar <sudhanshukumar8816@gmail.com>
2024-05-18 12:45:06 +0530

    to-do-app development

--- To-Do-App/script.js
+++ To-Do-App/script.js
@@ -1,11 +1,17 @@
 
 
-var addBtn = document.getElementsByClassName("add-item-btn")[0]
-var overlay = document.getElementsByClassName("overlay")[0]
-var closeBtn = document.getElementById("close-icon")
-var itemForm = document.getElementsByClassName("item-form")[0]
+const addBtn = document.getElementsByClassName("add-item-btn")[0]
+const overlay = document.getElementsByClassName("overlay")[0]
+const closeBtn = document.getElementById("close-icon")
+const itemForm = document.getElementsByClassName("item-form")[0]
+const itemCtn = document.getElementsByClassName("item-ctn")[0]
+const discardButton = document.getElementsByClassName("discard-btn")[0]
+const completeButton = document.getElementsByClassName("complete-btn")[0]
 
-
+let activeItemsArray = []
+let discardedItemsArray = []
+let completedItemsArray = []
+var itemCount = 2
 
 
 function openFormHandler(){
@@ -14,6 +20,12 @@ function openFormHandler(){
 
 function closeFormHandler(){
     overlay.classList.add("hidden")
+}
+
+
+function discardHandler(event){
+    console.log(event)
+    console.log(event.target.classList[0])
 }
 
 function formSubmitHandler(event){
@@ -25,6 +37,71 @@ function formSubmitHandler(event){
         startDate: event.target[2].value,
         endDate: event.target[3].value
     }
+
+
+    const item = document.createElement("div")
+    item.classList.add(String(itemCount))
+    item.classList.add("item")
+    
+    
+
+    const itemTitle = document.createElement("h1")
+    itemTitle.classList.add("title")
+    itemTitle.innerText = itemValues.itemTitle
+
+    const itemDesc = document.createElement("p")
+    itemDesc.classList.add("desc")
+    itemDesc.innerText = itemValues.itemDescription
+
+    const dateCtn = document.createElement("div")
+    dateCtn.classList.add("date-ctn")
+
+    const startDate = document.createElement("h2")
+    startDate.classList.add("date")
+    startDate.classList.add("start-date")
+    startDate.innerText = itemValues.startDate
+
+    const endDate = document.createElement("h2")
+    endDate.classList.add("date")
+    endDate.classList.add("end-date")
+    endDate.innerText = itemValues.endDate
+
+    dateCtn.appendChild(startDate)
+    dateCtn.appendChild(endDate)
+
+    const utilityBtnCtn = document.createElement("div")
+    utilityBtnCtn.classList.add("utility-btn-ctn")
+
+    const discardBtn = document.createElement("button")
+    discardBtn.classList.add(String(itemCount))
+    discardBtn.classList.add("btn")
+    discardBtn.innerText = "Discard"
+    discardBtn.classList.add("discard-btn")
+    discardBtn.addEventListener("click",discardHandler)
+
+    const completeBtn = document.createElement("button")
+    completeBtn.classList.add("btn")
+    completeBtn.innerText = "Mark As Complete"
+    completeBtn.classList.add("complete-btn")
+    utilityBtnCtn.appendChild(discardBtn)
+    utilityBtnCtn.appendChild(completeBtn)
+
+    item.appendChild(itemTitle)
+    item.appendChild(itemDesc)
+    item.appendChild(dateCtn)
+    item.appendChild(utilityBtnCtn)
+
+    activeItemsArray.push(item)
+    console.log(activeItemsArray)
+    itemCtn.appendChild(item)
+
+
+    itemCount+=1
+
+    console.log(itemTitle)
+
+
+
     console.log(itemValues)
     itemForm.reset()
 }
@@ -35,3 +112,4 @@ function formSubmitHandler(event){
 closeBtn.addEventListener("click",closeFormHandler)
 addBtn.addEventListener("click",openFormHandler)
 itemForm.addEventListener("submit",formSubmitHandler)
+discardButton.addEventListener("click",discardHandler)
