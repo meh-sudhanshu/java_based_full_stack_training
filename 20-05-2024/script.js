598152936789f5a8ce26262062f3de5e0ead94b2
sudhanshu kumar <sudhanshukumar8816@gmail.com>
2024-05-20 12:38:02 +0530

    weather app

---
+++ 20-05-2024/script.js
@@ -0,0 +1,143 @@
+// javascript ---> js for web development, js as a programming language
+
+
+// var,let, const
+
+// loops in js, objects in js, arrays, methods on arrays,
+// promise, fetch, async await, callback functions
+
+
+// two js apps ---> weather app, random joke generator
+
+
+
+// -----------------------arrays in js --------------------------
+
+
+// var arr = []
+// arr.push("hello")
+// arr.push("hi")
+// arr.push("hiiiiiii")
+// arr.push(20)
+// arr.push(12.4)
+// arr.push(100)
+
+// console.log(arr.length)
+
+// console.log(arr.includes(100))
+
+// console.log(arr)
+
+
+// var arr = [10,20,30,40,50,60]
+
+// for(var i=0;i<arr.length;i++){
+//     arr[i] = arr[i]*2
+// }
+// console.log(arr)
+
+// arr.map(ele => {
+//     console.log(ele*2)
+// })
+
+
+// var myArr = [2,5,13,24,56,78,98,76,45,23]
+// var oddArr = myArr.filter(ele => ele%2 === 1)
+// console.log(oddArr)
+
+// var oddArr = []
+// for(var i=0;i<myArr.length;i++){
+//     if(myArr[i]%2 !== 0){
+//         oddArr.push(myArr[i])
+//     }
+// }
+// console.log(oddArr)
+
+// push, length, map, filter, includes,
+
+// var arr2 = [2,3,4,1,8,9]
+
+// let i = 0
+// while(i<arr2.length){
+//     console.log(arr2[i])
+//     i+=1
+// }
+
+// do{
+//     console.log(arr2[i])
+//     i+=1
+// }while(I<arr2.length)
+
+
+// for(ele of arr2){
+//     console.log(ele)
+// }
+// for(index in arr2){
+//     console.log(arr2[index])
+// }
+
+
+
+
+// console.log(arr2)
+
+
+// for(var i=0; i<arr2.length;i++){
+//     // console.log(arr2[i])
+//     process.stdout.write(String(arr2[i])+" ")
+// }
+
+
+// while(false){
+//     console.log("I am Trying to execute a while loop with false condition")
+// }
+
+// do{
+//     console.log("I am Trying to execute a do while loop with false condition")
+// }while(false)
+
+// var,let,const, array, push, map, filter, includes, length
+// for-loop, for-in, for-of, while, do-while
+
+// var arr = [1,2,3]
+// function doJob(arg1,arg2,arg3,arg4){
+//     console.log(arg1,arg2,arg3,arg4)
+// }
+// arr.forEach(doJob)
+
+// callback function is a function that is being passed to another function as an argument
+
+// data types-> Number
+
+// var a = 10.2
+// var b = 100
+// var c = 89.0
+// console.log(typeof(a),typeof(b),typeof(c))
+
+//Number, String, Boolean, Undefined, null, Symbol, Object, Bigint
+function doJob(){
+    console.log("i am doing my job")
+}
+
+const obj = {
+    name:"Sudhanshu Kumar",
+    age:24.5,
+    height:"5ft 7inch",
+    getSudhanshuData: function (){
+        return "This is sudhanshu Data"
+    }
+}
+
+console.log(obj.name)
+console.log(obj.age)
+console.log(obj.getSudhanshuData())
+// method is a function that is associated with some object
+
+// promise ---> promise is a js object
+
+
+
+
+
+
+
---
+++ 20-05-2024/weather-app/script.js
@@ -0,0 +1,80 @@
+
+
+const searchBtn = document.getElementsByClassName("search-btn")[0]
+const infoCtn = document.getElementsByClassName("info-ctn")[0]
+const left = document.getElementsByClassName("left")[0]
+const right = document.getElementsByClassName("right")[0]
+const img = document.getElementsByClassName("card_icon")[0]
+const tempHeading = document.getElementsByClassName("temp")[0]
+const conditionHeading = document.getElementsByClassName("condition")[0]
+const cityHeading = document.getElementsByClassName("city")[0]
+
+
+
+function getValue(){
+
+    left.removeChild(document.getElementsByClassName("card_icon")[0])
+
+
+
+    const inputField = document.getElementsByClassName("ip")[0]
+    const cityName = (inputField.value)
+    const baseUrl = "http://api.weatherapi.com/v1/"
+    const apiKey = "bc85ca2527114f0d86b65521241801"
+
+    const api = baseUrl+"current.json?key="+apiKey+"&q="+cityName
+
+    const apiRequestData = {
+        method:"POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Header":"*"
+          },
+    }
+    const res = fetch(api,apiRequestData)
+    res.then(res => res.json()).then(data=>{
+        const condition = data.current.condition.text
+        const icon = data.current.condition.icon
+        const city = data.location.name
+        const state = data.location.region
+        const tempC = data.current.temp_c
+        img.src = icon
+        img.alt="Icon Not Found"
+        img.classList.add("card_icon")
+        img.classList.remove("hidden")
+        left.appendChild(img)
+
+        tempHeading.innerText = tempC+" deg"+" C"
+
+       
+        conditionHeading.innerText = condition
+        
+        
+       
+        cityHeading.innerText = city+","+state
+    
+
+        right.appendChild(tempHeading)
+        right.appendChild(conditionHeading)
+        right.appendChild(cityHeading)
+
+
+        infoCtn.appendChild(left)
+        infoCtn.appendChild(right)
+
+        console.log(condition)
+        console.log(icon)
+    }).catch(err=>{
+        console.log(err)
+    })
+    // console.log(res)
+
+
+
+
+    inputField.value = ""
+}
+
+
+
+searchBtn.addEventListener("click",getValue)
